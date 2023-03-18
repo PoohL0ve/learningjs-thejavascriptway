46e32e3be402b515d9d3c7c78160c45078fe57cb
PoohL0ve <screwed06@gmail.com>
2023-03-18 18:15:19 -0400

    using the console

---
+++ chapter1/ch1.js
@@ -0,0 +1,13 @@
+"use strict";
+// using modern javascript
+
+//using 6 and 3 for addition, multiplication, subtraction, division and remainder
+
+console.log(6 + 3);
+console.log(6 * 3);
+console.log(6 - 3);
+console.log(6 / 3);
+console.log(6 % 3);
+console.log(3 - 6);
+console.log(3 / 6);
+console.log(3 % 6);
