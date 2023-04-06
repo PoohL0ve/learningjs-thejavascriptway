7fe22e5d6cbb900e903e038f98d9c0db01822fd6
PoohL0ve <screwed06@gmail.com>
2023-04-06 01:25:31 -0400

    Simple two line code displaying each iteration of a for loop

---
+++ chapter4-loops/carousel/carousel.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//Using a for loop to run a carousel 10 times
+// Displaying each time in the console.
+
+for (let turns = 0; turns <= 10; turns++) {
+    console.log(turns);
+}
+
+ 
